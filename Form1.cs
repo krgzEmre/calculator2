2d35198e75bf18c4acd33f26ee9f1aa80a72b9e1
krgzEmre <126202233+krgzEmre@users.noreply.github.com>
2023-04-02 01:25:21 +0300

    calculator2

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayı1 = Convert.ToDouble(textBox1.Text);
+
+            double sayı2 = Convert.ToDouble(textBox2.Text);
+
+            if (checkBox1.Checked == true)
+            {
+                double sonuc = sayı1 + sayı2;
+
+                listBox1.Items.Add(sayı1 + "+" + sayı2 + "=" + sonuc);
+
+            }
+
+            if (checkBox3.Checked == true)
+            {
+                double sonuc = sayı1 - sayı2;
+
+                listBox1.Items.Add(sayı1 + "-" + sayı2 + "=" + sonuc);
+
+            }
+
+            if (checkBox4.Checked == true)
+            {
+                double sonuc = sayı1 * sayı2;
+
+                listBox1.Items.Add(sayı1 + "*" + sayı2 + "=" + sonuc);
+
+            }
+
+            if (checkBox2.Checked == true)
+            {
+                double sonuc = sayı1 / sayı2;
+                listBox1.Items.Add(sayı1 + "/" + sayı2 + "=" + sonuc);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
